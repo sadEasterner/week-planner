d154cb2ecfbaf88f2238e76e20b01464780b87cb
sadEasterner <hoseinseifig1380@gmail.com>
2023-08-25 21:07:55 +0330

    [Initail] : Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Fragment, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const App = () => {
+
+
+  return (
+    <Fragment>
+      
+    </Fragment>
+  )
+}
+
+
+export default App
